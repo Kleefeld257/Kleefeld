10d24ee8de41635c4fd4d9d763f0a87578764899
Kleefeld257 <fabianmetroxhey@gmail.com>
2026-06-08 14:26:48 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Kleefeld´s "Projekt"",
+        name: "Kleefeld´s Projekt",
         // Activity type number (0 = Playing).
         type: 0, 
       },
